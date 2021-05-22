9fd6838d9039daf951a183ca75739ef81732b3d5
Stephan Dörfler <stephan@while-false.de>
2021-05-22 19:20:20 +0200

    cleanup

--- ClientApp/src/components/Planner/Planner.tsx
+++ ClientApp/src/components/Planner/Planner.tsx
@@ -1,55 +1,78 @@
-import React from "react";
+import React from 'react';
+import { Session } from '../../store/events/session';
 
 import './Planner.scss';
 
 const Planner: React.FunctionComponent = () => {
-    // start mock data
+  // start mock data
   const now = new Date();
-  const past = new Date();
-  past.setMonth(now.getMonth() + 1);
-  const events = [
+  const future = new Date();
+  future.setMonth(now.getMonth() + 1);
+  const sessions: Session[] = [
     {
-      date: now.toLocaleDateString(),
-      topic: "Sample recording",
+      date: now,
+      topic: 'Sample recording',
       guests: [],
+      season: 1,
+      episode: 1,
+      number: 1,
     },
     {
-      date: past.toLocaleDateString(),
-      topic: "Sample interview",
+      date: future,
+      topic: 'Sample interview',
       guests: [
         {
-          name: "Jane Doe",
-          company: "Contoso",
-          title: "Queen of Chaos",
+          name: 'Jane Doe',
+          company: 'Contoso',
+          title: 'Queen of chaos',
         },
         {
-          name: "John Doe",
-          company: "Tailwind Traders",
-          title: "Master of desaster",
+          name: 'Edward Dillinger',
+          company: 'Tailwind Traders',
+          title: 'Master of desaster',
         },
       ],
+      season: 1,
+      episode: 2,
+      number: 2,
     },
   ];
   // end mock data
+  const mapGuests = (session: Session) => {
+    if (!session.guests || session.guests.length == 0) {
+      return <></>;
+    }
+    return (
+      <ul>
+        {session.guests.map((guest: any) => (
+          <li>
+            {guest.name} ({guest.title} at {guest.company})
+          </li>
+        ))}
+      </ul>
+    );
+  };
   return (
     <div className="upcoming-events">
+      <div className="today">Today is {new Date().toLocaleDateString()}</div>
       <ul>
-        {events.map((event) =>
+        {sessions.map((session: Session) => (
           <li>
-            {`${event.date}: ${event.topic}`}
-            {event.guests.length != 0 ? (
-              <ul>
-                {event.guests.map((guest: any) => (
-                  <li>
-                    {guest.name} ({guest.title} at {guest.company})
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <></>
-            )}
+            <div className="session-numbers">
+              {session.number.toLocaleString(undefined, {
+                minimumIntegerDigits: 3,
+              })}
+              : Sprint{' '}
+              {session.season.toLocaleString(undefined, {
+                minimumIntegerDigits: 2,
+              })}{' '}
+              - Feature{' '}
+              {session.episode}
+            </div>
+            {`${session.date.toLocaleDateString()}: ${session.topic}`}
+            {mapGuests(session)}
           </li>
-        )}
+        ))}
       </ul>
     </div>
   );

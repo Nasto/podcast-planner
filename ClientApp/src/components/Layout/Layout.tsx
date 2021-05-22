30a0a135330cfbb99e46a84ba320b95096bfc9ae
Stephan Dörfler <stephan@while-false.de>
2021-05-22 19:19:57 +0200

    added simple responsiveness

--- ClientApp/src/components/Layout/Layout.tsx
+++ ClientApp/src/components/Layout/Layout.tsx
@@ -1,19 +1,22 @@
 import * as React from 'react';
 import NavMenu from './NavMenu';
 
-import "./Layout.scss"
+import './Layout.scss';
 
-const Layout: React.FunctionComponent = ({children}) => {
-    return (
-        <>
-            <NavMenu />
-            <div className="app-wrapper">
-                <div className="app-content">
-                    {children}
-                </div>
-            </div>
-        </>
-    );
-}
+const Layout: React.FunctionComponent = ({ children }) => {
+  return (
+    <>
+      <div className="app-wrapper">
+        <NavMenu />
+        <div className="app-content">
+          <header>
+            <div className="title">Podcast Planner</div>
+          </header>
+          <div className="current-view">{children}</div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Layout;
--- ClientApp/src/components/Layout/NavMenu.tsx
+++ ClientApp/src/components/Layout/NavMenu.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
 
 import './NavMenu.scss';
 
@@ -11,14 +12,11 @@ const NavMenu: React.FunctionComponent = () => {
 
     return (
         <>
-            <header>
-                <div className="title">Podcast Planner</div>
-            </header>
             <div className="menu">
                 <div className="button-wrapper">
-                    <button onClick={toggle} className="menu-button">toggle</button>
+                    <button onClick={toggle} className="menu-button"><FaBars /></button>
                 </div>
-                <div className="menu-items">
+                <div className={`menu-items ${isOpen ? 'open' : 'closed'}`}>
                     {isOpen ? <span>Menu</span> : <span>M</span>}
                     {isOpen ? <span>Long menu item</span> : <span>L</span>}
                 </div>
